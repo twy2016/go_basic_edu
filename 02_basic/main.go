5654e97a56ff764e1715b15ec7919cac4cb247c3
gongpeng <gongpeng@huayun.com>
2022-08-10 22:18:19 +0800

    feat: 基础学习

--- 02_basic/main.go
+++ 02_basic/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
+const name = "唐万言"
+
+func main() {
+	var i = 10
+	fmt.Println(i)
+	var f32 float32 = 2.1
+	var f64 float64 = 2.14159
+	fmt.Println(f32)
+	fmt.Println(f64)
+	fmt.Println(name)
+	fmt.Printf("类型：%T\n", f32)
+	fmt.Printf("%v\n", f32)
+	fmt.Printf("%+v\n", f32)
+	fmt.Printf("%#v\n", f32)
+
+	//Itoa函数可以把一个 int 类型转为 string
+	i2s := strconv.Itoa(i)
+	//Atoi函数可以把一个 string 类型转为 int
+	s2i, err := strconv.Atoi(i2s)
+	fmt.Println(i2s, s2i, err)
+
+	s1 := "Hello"
+	//判断s1的前缀是否是H
+	fmt.Println(strings.HasPrefix(s1, "H"))
+	//在s1中查找字符串o
+	fmt.Println(strings.Index(s1, "o"))
+	//把s1全部转为大写
+	fmt.Println(strings.ToUpper(s1))
+	//把s1全部转为小写
+	fmt.Println(strings.ToLower(s1))
 }
--- 03_flow/main.go
+++ 03_flow/main.go
@@ -1,5 +1,68 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//if
+	i := 10
+	if i > 10 {
+		fmt.Println("i>10")
+	} else {
+		fmt.Println("i<=10")
+	}
+
+	if i := 6; i > 10 {
+		fmt.Println("i>10")
+	} else if i >= 6 && i <= 10 {
+		fmt.Println("i>=6&&i<=10")
+	} else {
+		fmt.Println("i<10")
+	}
+
+	//switch
+	switch i := 7; {
+	case i > 10:
+		fmt.Println("i>10")
+	case i > 5 && i <= 10:
+		fmt.Println("5<i<=10")
+	default:
+		fmt.Println("i<=5")
+	}
+
+	//默认每个case自带break,fallthrough
+	switch j := 1; j {
+	//case "a":
+	case 1:
+		fmt.Println(fmt.Sprintf("%s%d", "case1:", j))
+		fallthrough
+	case 2:
+		fmt.Println(fmt.Sprintf("%s%d", "case2:", j))
+	default:
+		fmt.Println("没有匹配")
+	}
+
+	//for循环
+	sum := 0
+	for i := 1; i <= 100; i++ {
+		sum += i
+	}
+	fmt.Println("the sum is", sum)
+
+	//无限循环
+	//for {
+	//	fmt.Println("i")
+	//}
+
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			if j == 2 {
+				// 设置退出标签
+				goto breakTag
+			}
+			fmt.Printf("%v-%v\n", i, j)
+		}
+	}
+	return
+breakTag:
+	fmt.Println("结束for循环")
 }
